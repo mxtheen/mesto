e78ed009f4e378a783ccc173911d0f19bbbefab3
Maxim Shuliaev <maximshuliaev@gmail.com>
2023-01-25 10:37:37 +0300

    commit checkx2

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-// Находим форму в DOM
-let formElement = // Воспользуйтесь методом querySelector()
+let popupBtnOpen = document.querySelector(".profile__edit-button");
+
+let popBtnClose = document.querySelector(".popup__close-button");
+
+let popupContainer = document.querySelector(".popup");
+
+popupBtnOpen.addEventListener("click", openPopup);
+popBtnClose.addEventListener("click", closePopup);
+
+function openPopup () {
+  popupContainer.classList.add("popup__opened")
+}
+
+function closePopup() {
+  popupContainer.classList.remove("popup__opened")
+}
+
+
+// Находим форму в DOM //
+let formElement = document.querySelector(".popup__form") // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let nameInput = // Воспользуйтесь инструментом .querySelector()
-let jobInput = // Воспользуйтесь инструментом .querySelector()
+let nameInput = document.querySelector(".popup__input_name") // Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector(".popup__input_profession") // Воспользуйтесь инструментом .querySelector()
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -21,3 +39,4 @@ function handleFormSubmit (evt) {
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
+
