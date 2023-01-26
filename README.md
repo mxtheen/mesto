24b9089eb708699f06fdcdfcbea98702e6f4ddd1
Maxim Shuliaev <maximshuliaev@gmail.com>
2023-01-26 20:25:06 +0300

    commit finish work

--- README.md
+++ README.md
@@ -1,17 +1,52 @@
 # Проект: Место
 
+
+Mesto - это проектная работы от платформы "Яндекс.Практикум", в которой впервые за время обучения применяются основы JavaScript для реализации "попапов"
+
+Работа подразумевает собой **одностраничный сайт** с использованием различных **CSS3** и **HTML5** технологий, а также базовых технологий **JavaScript**.
 ### Обзор
+Организация файловой структуры выполнена по технологии Nested, а сама работа использует методологию БЭМ для разделения интерфейса на независимые блоки.
+```
+mesto
+    blocks
+      element
+      elements
+      footer
+      header
+      page
+      popup
+      profile
+```
 
-* Figma
-* Картинки
+Все стили для данной работы подключены в отдельном файле **index.css**, который в свою очередь находится в папке **pages**
 
-**Figma**
+В данной работе используется нестандартный шрифт [Inter](https://rsms.me/inter/), который был подключен локально и находится в папке **fonts**. В этой папке присутствуют только те начертания, которые используются непосредственно в самой работе. Это позволяет не нагружать загрузку страницы лишней подкачкой шрифтов, которые не используются в работе.
 
-* [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+Используются оптимизированные изображения, которые находится в папке **images**. Оптимизация изображений также важна, чтобы не нагружать сайт лишней загрузкой больших изображений.
 
-**Картинки**
+В работе, как было сказано выше, впервые применяется язык JavaScript. Работа этого языка подключена в файле **index.js**, который находится в корне папки **mesto**.
 
-Доставать картинки предстоит из Фигмы. Это расхожая практика, поэтому полезно потренироваться.
-Не забудьте [оптимизировать картинки](https://tinypng.com/), чтобы ваш сайт загружался быстрее.
+```
+let popupContainer = document.querySelector(".popup");
+let popupBtnOpen = document.querySelector(".profile__edit-button");
+let popBtnClose = document.querySelector(".popup__close-button");
 
-Удачи!
+function openPopup () {
+  popupContainer.classList.add("popup__opened");
+  nameInput.value = profileTitle.textContent;
+  jobInput.value= profileSubtitle.textContent;
+}
+
+function closePopup() {
+  popupContainer.classList.remove("popup__opened");
+}
+
+popupBtnOpen.addEventListener("click", openPopup);
+popBtnClose.addEventListener("click", closePopup);
+```
+Выше приведен пример реализации открытия попапов с помощью переменных и простейших функций.
+
+[Ссылка на GitHub Pages тут.](https://mxtheen.github.io/mesto/)
+
+>Максим Шуляев
+
--- index.js
+++ index.js
@@ -1,42 +1,44 @@
-let popupBtnOpen = document.querySelector(".profile__edit-button");
-
-let popBtnClose = document.querySelector(".popup__close-button");
-
 let popupContainer = document.querySelector(".popup");
+// Объявляем переменную popupContainer //
+let popupBtnOpen = document.querySelector(".profile__edit-button");
+// Объявляем переменную popupBtnOpen //
+let popBtnClose = document.querySelector(".popup__close-button");
+// Объявляем переменную popBtnClose //
+let formElement = document.querySelector(".popup__form");
+// Объявляем переменную formElement //
+let nameInput = document.querySelector(".popup__input_name");
+// Объявляем переменную nameInput //
+let jobInput = document.querySelector(".popup__input_profession");
+// Объявляем переменную jobInput //
+let profileTitle = document.querySelector (".profile__title");
+// Объявляем переменную profileTitle //
+let profileSubtitle = document.querySelector (".profile__subtitle");
+// Объявляем переменную profileSubtitle //
 
-popupBtnOpen.addEventListener("click", openPopup);
-popBtnClose.addEventListener("click", closePopup);
 
 function openPopup () {
-  popupContainer.classList.add("popup__opened")
+  popupContainer.classList.add("popup__opened");
+  nameInput.value = profileTitle.textContent;
+  jobInput.value= profileSubtitle.textContent;
 }
-
+// Объявляем функцию openPopup, которая отвечает за открытие всплывающего окна //
 function closePopup() {
-  popupContainer.classList.remove("popup__opened")
+  popupContainer.classList.remove("popup__opened");
 }
+// Объявляем функцию closePopup, которая отвечает за закрытие всплывающего окна //
+popupBtnOpen.addEventListener("click", openPopup);
+// Вызываем функцию openPopup с методом addEventListener, которая отвечает за открытие всплывающего окна //
+popBtnClose.addEventListener("click", closePopup);
+// Вызываем функцию closePopup с методом addEventListener, которая отвечает за открытие всплывающего окна //
 
-
-// Находим форму в DOM //
-let formElement = document.querySelector(".popup__form") // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = document.querySelector(".popup__input_name") // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector(".popup__input_profession") // Воспользуйтесь инструментом .querySelector()
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function handleFormSubmit (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
+    evt.preventDefault();
+    profileTitle.textContent= nameInput.value;
+    profileSubtitle.textContent= jobInput.value;
+    popupContainer.classList.remove("popup__opened");
 }
+// Объявляем функцию handleFormSubmit, которая отвечает за получение значений поля jobInput и nameInput и вставляет новые значения с помощью textContent в блоки profile.Title и profile.Subtitle //
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
+//Добавляем formElement с методом addEventListener, которая при взаимодействии с кнопкой типа "submit" выполняет функцию handleFormSubmit //
 
