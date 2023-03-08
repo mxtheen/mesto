0a7c926990815ed7dc0f3ac3b89b9c9cb7d38289
Maxim Shuliaev <maximshuliaev@gmail.com>
2023-03-09 01:26:09 +0300

    коммит для теста

--- scripts/index.js
+++ scripts/index.js
@@ -24,7 +24,7 @@ const initialCards = [
     link: 'https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
   }
 ];
-
+const popup = document.querySelectorAll(".popup")
 const popupEdit = document.querySelector(".popup_edit")
 const popupAdd = document.querySelector(".popup_add")
 const popupScaleImage = document.querySelector(".popup_scale-image")
@@ -35,29 +35,45 @@ const popupBtnCloseEdit = document.querySelector(".popup__close-button_edit")
 const popupBtnCloseScale = document.querySelector(".popup__close-button_image")
 const cardTemplate = document.querySelector("#card-template").content.querySelector(".element")
 const elements = document.querySelector(".elements")
-const formElementEdit = document.querySelector(".popup__form-edit");
-const formElementAdd = document.querySelector(".popup__form-add");
+const formEdit = document.forms.form__edit;
+const formAdd = document.forms.form__add;
+const nameInput = formEdit.elements.name
+const jobInput = formEdit.elements.profession
+const titleInput = formAdd.elements.title
+const linkInput = formAdd.elements.link
 const submitEdit = document.querySelector(".popup__save-button_edit")
 const submitAdd = document.querySelector(".popup__save-button_add")
 const profileTitle = document.querySelector(".profile__title")
 const profileSubtitle = document.querySelector(".profile__subtitle")
-const nameInput = document.querySelector(".popup__input_type_name")
-const jobInput = document.querySelector(".popup__input_type_job")
-const titleInput = document.querySelector(".popup__input_type_title")
-const linkInput = document.querySelector(".popup__input_type_link")
 const popupCaption = document.querySelector('.popup__caption')
 const popupImage = document.querySelector('.popup__image')
 
-
 function openPopup (item) {
   item.classList.add("popup_opened")
+  item.addEventListener("click", overlayClosePopup)
 }
-
 
 
 function closePopup (item) {
  item.classList.remove("popup_opened")
+ item.removeEventListener("click", overlayClosePopup)
 }
+
+function escapeClosePopup (evt) {
+  const popupOpened = document.querySelector(".popup_opened")
+  if (evt.key === "Escape") {
+    closePopup(popupOpened)
+    document.removeEventListener("keydown", escapeClosePopup)
+  }
+}
+
+function overlayClosePopup(evt) {
+  const popupOpened = document.querySelector(".popup_opened")
+  if (evt.target === evt.currentTarget) {
+    closePopup(popupOpened);
+  };
+}
+
 
 function renderCards (items) {
   const cards = items.map ((item) => {
@@ -85,19 +101,20 @@ function createCard (item) {
     popupImage.src = cardElement.querySelector(".element__image").src
     popupImage.alt = item.name
     openPopup(popupScaleImage)
+    document.addEventListener("keydown", escapeClosePopup)
   })
   return cardElement
 }
 
 
-formElementAdd.addEventListener("submit", (evt) => {
+formAdd.addEventListener("submit", (evt) => {
   evt.preventDefault()
   const cardElement = createCard ({name: titleInput.value, link: linkInput.value});
   elements.prepend(cardElement)
   closePopup(popupAdd)
 })
 
-formElementEdit.addEventListener("submit", (evt) => {
+formEdit.addEventListener("submit", (evt) => {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
@@ -110,13 +127,15 @@ popupBtnAdd.addEventListener("click", (function () {
   titleInput.value = titleInput.textContent
   linkInput.value = linkInput.textContent
   openPopup(popupAdd)
+  document.addEventListener("keydown", escapeClosePopup)
 }))
 
 popupBtnEdit.addEventListener("click", function () {
   nameInput.value = profileTitle.textContent
   jobInput.value = profileSubtitle.textContent
-  openPopup(popupEdit)}
- )
+  openPopup(popupEdit)
+  document.addEventListener("keydown", escapeClosePopup)
+})
 
 
 popupBtnCloseAdd.addEventListener("click", ()=> closePopup(popupAdd))
---
+++ scripts/validate.js
@@ -0,0 +1,55 @@
+ const enableValidationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save-button',
+  inactiveButtonClass: 'popup__save-button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+};
+
+function enableValidation(config) {
+  const formList = Array.from(document.querySelectorAll(config.formSelector))
+  formList.forEach ((form) => {
+    form.addEventListener("input", () =>{
+      toggleButton(form, config)
+    })
+    addInputListeners(form, config)
+    toggleButton(form, config)
+  })
+
+}
+enableValidation(enableValidationConfig)
+
+
+function handleFormInput (evt, config) {
+  const input = evt.target;
+  const inputId = input.id
+  const errorSpan = document.querySelector(`#${inputId}-error`)
+
+  if(!input.validity.valid) {
+    input.classList.add(config.inputErrorClass)
+    errorSpan.textContent = input.validationMessage
+
+  } else {
+    input.classList.remove(config.inputErrorClass)
+    errorSpan.textContent = " "
+  }
+}
+
+function toggleButton (form, config) {
+  const buttonSubmit = form.querySelector(config.submitButtonSelector)
+  const isFormValid = form.checkValidity()
+  buttonSubmit.disabled = !isFormValid
+  buttonSubmit.classList.toggle(config.inactiveButtonClass, !isFormValid)
+}
+
+function  addInputListeners (form, config) {
+  const inputList = Array.from(form.querySelectorAll(config.inputSelector))
+  inputList.forEach((item)=>{
+    item.addEventListener ("input", (evt) => {
+      handleFormInput(evt, config)
+    })
+  })
+
+}
+
